d4517aa8bb72338dfc861c0efef9036ff0ac43c0
Gill <gillvelleda@gmail.com>
2019-11-12 23:58:48 -0300

    Remove folders

--- lista8/exer08_4.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-
-    for (int i=100;i<=200;i+=10)
-        printf(" %d",i);
-    printf("\n");
-    return 0;
-}
--- lista9/exer09_8.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-    int i,s;
-    for(i=1;s<=100;i++,s+=i);
-    printf("%d\n",i);
-}
