2e0a05a80e73253a86d7b8398577a0ff6f8a3c82
Gill <gillvelleda@gmail.com>
2019-11-12 23:58:16 -0300

    Remove folders

--- aula14/conio-for-linux-master/build/Debug/GNU-Linux-x86/conio.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux-x86/conio.o: conio.c conio.h
-
-conio.h:
--- lista12/exer12_02.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(void){
-    int v[10],i;
-    for(i=0;i<10;i++)v[i]=i+101;
-
-    for(i=0;i<10;i++)printf("%d ",v[i]);
-    printf("\n");
-    return 0;
-}
